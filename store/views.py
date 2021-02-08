5ec6d533065e6feda924129257a73bec6b6446e8
huntingmarco <laquihonjannel@yahoo.com>
2021-02-07 21:26:32 -0500

    create reusable cartdata in views

--- store/views.py
+++ store/views.py
@@ -4,81 +4,32 @@ import json
 import datetime
 
 from .models import *
-from .utils import cookieCart
+from .utils import cookieCart, cartData
 
 # Create your views here.
 def store(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-		cartItems = order.get_cart_items
-	else:
-		items = []
-		order = {'get_cart_total':0,'get_cart_items':0,'shipping':False} #creating empty dictionary
-		cartItems = order['get_cart_items']
+	data = cartData(request)
+	cartItems = data['cartItems']
 
 	products = Product.objects.all()
 	context = {'products':products,'cartItems':cartItems}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-		cartItems = order.get_cart_items
-	else:
-		try:
-			cart = json.loads(request.COOKIES['cart'])
-		except:
-			cart = {}
-		print('Cart:', cart)
-		items = []
-		order = {'get_cart_total':0,'get_cart_items':0,'shipping':False} #creating empty dictionary
-		cartItems = order['get_cart_items']
-
-		for i in cart:
-			# try block to prevent items in cart that may have been removed from database
-			try:
-				cartItems += cart[i]["quantity"]
-
-				product = Product.objects.get(id=i)
-				total = (product.price * cart[i]['quantity'])
-
-				order['get_cart_total'] += total
-				order['get_cart_items'] +=cart[i]['quantity']
-
-				item = {
-					'product': {
-						'id':product.id,
-						'name':product.name,
-						'price':product.price,
-						'imageURL':product.imageURL
-						},
-					'quantity':cart[i]['quantity'],
-					'get_total':total,
-				}
-				items.append(item)
-
-				if product.digital == False:
-					order['shipping'] = True
-			except:
-				pass
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
 
 	context = {'items':items, 'order':order,'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-		cartItems = order.get_cart_items
-	else:
-		items = []
-		order = {'get_cart_total':0,'get_cart_items':0,'shipping':False} #creating empty dictionary
-		cartItems = order['get_cart_items']
+	
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
 
 	context = {'items':items, 'order':order,'cartItems':cartItems}
 	return render(request, 'store/checkout.html', context)
